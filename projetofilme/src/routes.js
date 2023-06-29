0dce14424569dcc0b928f198befc98d3a27a1931
Rodrigo De Sousa Pereira <rodrigo25sousa@gmail.com>
2023-06-29 10:05:30 -0300

    slider add to routes.js

--- projetofilme/src/routes.js
+++ projetofilme/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Favoritos from './pages/Favoritos';
+import Slider from './pages/Slider'
 
 import Header from './components/Header';
 import Footer from './components/Footer';
